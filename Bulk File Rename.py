65d7baafeccbae585298551acdc1461d6540e1a5
Alfredo <76714792+aalejo92@users.noreply.github.com>
2021-05-23 12:23:57 -0400

    Update Bulk File Rename.py
    05-23-21 - Cleaned code and added annotations

--- Bulk File Rename.py
+++ Bulk File Rename.py
@@ -4,7 +4,9 @@ import os
 # this function finds the path then renames each item in the folder starting with 0
 def main():
     i = 0
+    # local directory on computer for Python to find folder (note: change to fit your computer path)
     path = 'C:/Users/Alfredo/Desktop/Test Folder/'
+    # for loop to iterate entire folder
     for filename in os.listdir(path):
         # name & extension format can be changes here
         my_dest = 'File' + str(i) + '.rtf'
@@ -13,3 +15,7 @@ def main():
         os.rename(my_source, my_dest)
         i += 1
 
+
+# if statement to automatically run above function
+if __name__ == '__main__':
+    main()
